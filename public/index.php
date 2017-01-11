816cf07683c6cb0b48988faa593830dc51bbbfbf
matt <mwitt@trainsweet.com>
2017-01-11 15:44:44 -0800

    Added .env testing stuff

--- public/index.php
+++ public/index.php
@@ -1,8 +1,15 @@
+<?php
+	require __DIR__ . '/../vendor/autoload.php';
+	$dotenv = new Dotenv\Dotenv(__DIR__.'/../');
+	$dotenv->load();
+?>
+
 <html>
 <head>
 </head>
 <body>
 	<h1>Hello Ricky!!!</h1>
+	<h5>This is my ENV test variable value: <?php echo getenv('TEST_VARIABLE'); ?></h5>
 </body>
 </html>
 
